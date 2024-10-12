7de854d0f07541349ad0060494574ff9c04a1777
Javier Campos <camposjavier143@gmail.com>
2024-10-11 22:41:47 -0300

    feat: metodo para leer entradas

--- src/main.c
+++ src/main.c
@@ -1,11 +1,22 @@
 #include "DFS.h"
 #include <stdio.h>
 #include <stdbool.h>
+#define MAXBUFFER 1024
 
 // FLAGS -1
 
 int main(){
     char *ruta_archivo = "./grafo.txt";
     FILE *archivo = fopen(ruta_archivo, "r");
-    
+    char *BUFFER[MAXBUFFER];
+
+    int contador;
+
+    if(fgets(BUFFER, sizeof(BUFFER), archivo) != NULL) 
+        contador = atoi(BUFFER);
+    else return 0;
+
+    while(fgets(BUFFER, sizeof(BUFFER), archivo) != NULL){
+        
+    }
 }
